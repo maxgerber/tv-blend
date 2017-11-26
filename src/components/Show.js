0789df0fbb67c68a0682c2186c683de1eb808e5b
Max Gerber <mgerber94@gmail.com>
2017-11-26 18:16:09 +0000

    showing basic information from the Show API #14

--- src/components/Show.js
+++ src/components/Show.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import CSSModules from 'react-css-modules';
 
+import removeHTML from '../helpers/removeHTML';
+import arrayToList from '../helpers/arrayToList';
+
 import styles from '../styles/Show.css';
 
 const Show = ({ contents }) => {
   return (
-    !contents ?
-      <section styleName="show">Loading...</section>
+    !contents
+      ? <section styleName="show">Loading...</section>
       :
       <section styleName="show">
-      Hey, {'' || contents.name}
-      </section>
-  );
+        <img src={contents.image.medium} alt={contents.name} />
+        <h1>{'' || contents.name}</h1>
+        <p>{removeHTML(contents.summary)}</p>
+        <table>
+          <tr>
+            <th>Streamed on</th>
+            <td>{contents.network.name}</td>
+          </tr>
+          <tr>
+            <th>Schedule</th>
+            <td>{arrayToList(contents.schedule.days)}</td>
+          </tr>
+          <tr>
+            <th>Status</th>
+            <td>{contents.status}</td>
+          </tr>
+          <tr>
+            <th>Genres</th>
+            <td>{arrayToList(contents.genres)}
+            </td>
+          </tr>
+        </table>
+
+      </section>);
 };
 
 export default CSSModules(Show, styles);
---
+++ src/helpers/arrayToList.js
@@ -0,0 +1,5 @@
+export default (arr) => {
+  if (Array.isArray(arr) === true && arr.length > 0) {
+    return arr.join(', ');
+  } return '¯\\_(ツ)_/¯';
+};
